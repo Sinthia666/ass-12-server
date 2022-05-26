b7d8ade224f6062a459d43b9342f004794d292d7
Sinthia666 <sinthia.rupa71@gmail.com>
2022-05-26 19:57:26 +0600

    error solved

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.owa0my4.mongodb.net/?retryWrites=true&w=majority`;
-
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
